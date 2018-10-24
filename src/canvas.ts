ad12b9e70b59f0fde15abc4afcabc5509fb048b1
Will <will@WileECoyote>
2018-10-24 09:51:03 -0400

    wobbly lines

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,15 +1,27 @@
 import * as THREE from "three";
+import vertexShader from "./vertexShader";
 
 interface ScreenSize {
   width: number;
   height: number;
 }
 
+const uniforms = {
+  time: { type: "f", value: 0 },
+  color: { value: new THREE.Color(0xffffff) }
+};
+
+const fragmentShader = `
+uniform mediump vec3 color;
+void main() {
+    gl_FragColor = vec4( color, 1.0);
+}`;
+
 export default ({ canvas, screenSize }: { canvas: HTMLDivElement, screenSize: ScreenSize }) => {
   const scene = buildScene();
   const renderer = buildRender(screenSize);
   const camera = buildCamera(screenSize);
-  createSceneSubjects(scene);
+  const sceneObjects = createSceneSubjects(scene);
 
   function buildScene(): THREE.Scene {
     return new THREE.Scene();
@@ -33,14 +45,38 @@ export default ({ canvas, screenSize }: { canvas: HTMLDivElement, screenSize: Sc
   }
 
   function createSceneSubjects(scene: THREE.Scene) {
+    const lineDiff = 0.2;
+    const rowDiff = 0.05;
+    const multiplier = 64.1;
+    const material = new THREE.RawShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: vertexShader({ lineDiff, rowDiff, multiplier }),
+      fragmentShader
+    });
+
+    const lines = [];
+    for (let x = 0; x < 30; x++) {
+      var curve = new THREE.SplineCurve([
+        new THREE.Vector2(-25, 3 * x - 50),
+        new THREE.Vector2(25, 3 * x - 25)
+      ]);
+      var points = curve.getPoints(150);
+      var geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+      // Create the final object to add to the scene
+      var splineObject = new THREE.Line(geometry, material);
+      scene.add(splineObject);
+      lines.push(splineObject);
+    }
 
     const light = new THREE.AmbientLight(0x404040);
     light.position.set(10, 0, 25);
     scene.add(light);
 
-    return { light };
+    return { material, lines, light };
   }
   function update(time: number) {
+    sceneObjects.material.uniforms.time.value = time / 10000;
     renderer.render(scene, camera);
   }
   return {
